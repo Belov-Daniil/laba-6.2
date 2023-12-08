c58a2fb9b78d2ee27477f10641c24a051ae4cb99
Belov-Daniil <133764038+Belov-Daniil@users.noreply.github.com>
2023-12-08 17:28:07 +0400

    6.2.py

--- 6.2.py
+++ 6.2.py
@@ -1,4 +1,4 @@
-#. У няни 7 разных фруктов (ф1,…ф7).
+# . У няни 7 разных фруктов (ф1,…ф7).
 # Сформировать (вывести) все возможные варианты меню полдника
 # (1 фрукт) для ребенка на неделю.
 # Доп. условие
@@ -25,20 +25,28 @@ def rec(a, k):
 
 
 def start():
-    print('У няни 7 разных фруктов (ф1,…ф7). Сформировать (вывести) все возможные варианты меню полдника (1 фрукт) для ребенка на неделю.')
-    print('Спустя некоторое время няня поняла что если поставить в распивании киви после яблока а ананас до то у ребёнка несварение. Поэтому  ')
-    print('киви должно стоять в неделе раньше яблока а ананас после')
+    print(
+        'У няни 7 разных фруктов (ф1,…ф7). Сформировать (вывести) все возможные варианты меню полдника (1 фрукт) для ребенка на неделю.')
     a = input('Напишите rec для рекурсивного решения, iter для итеративного \n')
     while a != 'rec' and a != 'iter':
         a = input('Напишите rec для рекурсивного решения, iter для итеративного \n')
     if a == 'rec':
+        print(
+            'В качестве усложнения составления расписания напишите три фрукта из представленных на выбор:'
+            ' apple, pineapple, orange, banana, pear, kiwi, avocado'
+            ' Они будут стоять в меню таким обазом - Фрукт 1 будет всегда до фрукта 2, а фрукт 3 будет после фрукта 2')
+        fruits_name = input().split(' ')
+        while len(fruits_name) < 3 or fruits_name[1] not in fruit_basket or fruits_name[0] \
+                not in fruit_basket or fruits_name[2] not in fruit_basket:
+            fruits_name = input(' напишите три фрукта из представленных на выбор: apple, pineapple,'
+                                ' orange, banana, pear, kiwi, avocado\n').split(' ')
         p = 0
         fr = rec(fruit_basket, 7)
         for j in fr:
             if len(np.unique(j)) < 7:
                 pass
             else:
-                if j.index('kiwi') < j.index('apple') > j.index('pineapple'):
+                if not (j.index(fruits_name[0]) < j.index(fruits_name[1])  > j.index(fruits_name[2])):
                     pass
                 else:
                     p += 1
@@ -52,12 +60,22 @@ def start():
         else:
             exit()
     else:
+        print(
+            'В качестве усложнения составления расписания напишите три фрукта из представленных на выбор:'
+            ' apple, pineapple, orange, banana, pear, kiwi, avocado'
+            ' Они будут стоять в меню таким обазом - Фрукт 1 будет всегда до фрукта 2, а фрукт 3 будет после фрукта 2')
+        fruits_name = input().split(' ')
+        while len(fruits_name) < 3 or fruits_name[1] not in fruit_basket or fruits_name[0] \
+                not in fruit_basket or fruits_name[2] not in fruit_basket:
+            fruits_name = input(' напишите три фрукта из представленных на выбор: apple, pineapple,'
+                                ' orange, banana, pear, kiwi, avocado\n').split(' ')
         p = 0
         fi = iter()
         for j in fi:
-            if j.index('kiwi') < j.index('apple') > j.index('pineapple'):
-                pass
+            if not (j.index(fruits_name[0]) < j.index(fruits_name[1]) < j.index(fruits_name[2])):
+                continue
             else:
+                print(j.index(fruits_name[0]) < j.index(fruits_name[1]) < j.index(fruits_name[2]))
                 p += 1
                 print(*j)
         print('общее кол-во вариантов меню на неделю : ' + str(p))
@@ -68,5 +86,6 @@ def start():
             start()
         else:
             exit()
+
 
 start()
